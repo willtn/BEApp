864449d0e11790bc13ad628fe975eb2db8d8abfc
WillNguyen <trung.nguyen.14@hotmail.com>
2013-11-01 23:03:09 -0700

    Included dist folder

---
+++ dist/scripts/80b62067.scripts.js
@@ -0,0 +1,1 @@
+"use strict";angular.module("BlockExplorerApp",["ngCookies","ngResource","ngSanitize"]).config(["$routeProvider",function(a){a.when("/",{templateUrl:"views/main.html",controller:"MainCtrl"}).otherwise({redirectTo:"/"})}]),angular.module("BlockExplorerApp").controller("MainCtrl",["$scope",function(a){a.awesomeThings=["HTML5 Boilerplate","AngularJS","Karma"]}]);
