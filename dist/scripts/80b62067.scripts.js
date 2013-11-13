013ec5ae2e6095f1f353fc197a2436479a68d140
WillNguyen <trung.nguyen.14@hotmail.com>
2013-11-13 11:08:31 -0800

    remove

--- dist/scripts/80b62067.scripts.js
+++
@@ -1,1 +0,0 @@
-"use strict";angular.module("BlockExplorerApp",["ngCookies","ngResource","ngSanitize"]).config(["$routeProvider",function(a){a.when("/",{templateUrl:"views/main.html",controller:"MainCtrl"}).otherwise({redirectTo:"/"})}]),angular.module("BlockExplorerApp").controller("MainCtrl",["$scope",function(a){a.awesomeThings=["HTML5 Boilerplate","AngularJS","Karma"]}]);
