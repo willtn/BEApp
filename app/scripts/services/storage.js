4f0edfe7df574279ef3b733add269468765b4840
WillNguyen <trung.nguyen.14@hotmail.com>
2013-11-14 18:32:23 -0800

    Implemented Abstract Firebase Storage Service; Start Firebase branch to integrate service

--- app/scripts/services/storage.js
+++ app/scripts/services/storage.js
@@ -3,4 +3,19 @@
  */
 'use strict';
 
-services.factory('blockStorage', []);
+/**
+ * Abstract Firebase storage service
+ */
+services.factory('blockStorage', [function(angularFire) {
+  var _url = null,
+    _ref = null;
+  return {
+    init: function (url){
+      _url = url;
+      _ref = new Firebase(_url);
+    },
+    setToScope: function (scope, localScopeVarName) {
+      angularFire(_ref, scope, localScopeVarName);
+    }
+  }
+}]);
